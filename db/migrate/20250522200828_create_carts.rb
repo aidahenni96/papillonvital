b3e4e31f2ab4bf259a703aee45986d147258fdfa
aidahenni96 <aidahenni96@icloud.com>
2025-05-22 22:12:23 +0200

    cart and cartproduct

---
+++ db/migrate/20250522200828_create_carts.rb
@@ -0,0 +1,11 @@
+class CreateCarts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :carts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :status
+      t.decimal :total_price
+
+      t.timestamps
+    end
+  end
+end
